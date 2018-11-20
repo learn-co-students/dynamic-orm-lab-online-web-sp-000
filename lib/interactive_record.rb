0ad80535709aa73df334e9c8c7264658d61221a7
Austin  Burke <aburk3@gmail.com>
2018-11-20 12:42:44 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,17 +2,27 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
+
+  def initialize(options={})
+    options.each do |property, value|
+      self.send("#{property}=", value)
+    end
+  end
+  
   def self.table_name
     self.to_s.downcase.pluralize
   end
 
   def self.column_names
-    sql = <<-SQL
-      SELECT *
-      FROM students
-    SQL
+    sql = "PRAGMA table_info('#{table_name}')"
 
-    DB[:conn].execute(sql)
+    table_info = DB[:conn].execute(sql)
+    column_names = []
+
+    table_info.each do |column|
+      column_names << column["name"]
+    end
+    column_names.compact
   end
-  
+
 end
