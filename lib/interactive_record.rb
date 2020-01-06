12db54ff18279063c7c402a324f801a0864725ba
Anthony Tilelli <anthony@tilelli.me>
2020-01-06 03:17:29 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,5 +1,6 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require 'pry'
 
 class InteractiveRecord
   
@@ -16,12 +17,15 @@ class InteractiveRecord
   end
 
   def save
-    DB[:conn].execute("INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (?)", [values_for_insert])
-   
+    sql = <<-SQL
+    INSERT INTO #{table_name_for_insert}  ( #{col_names_for_insert} )
+    VALUES (#{values_for_insert})
+    SQL
+    # binding.pry
+    DB[:conn].execute(sql)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
-    
-  
+
   def values_for_insert
     self.class.column_names.map do |col_name|
       "'#{send(col_name)}'" if !send(col_name).nil?
@@ -45,5 +49,18 @@ class InteractiveRecord
  
     column_names.compact
   end
+  
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT * FROM #{self.table_name} 
+    WHERE name = ?
+    SQL
+    # binding.pry
+    DB[:conn].execute(sql, name)
+  end
+  
+  def self.find_by(hsh)
+  end
+  
   
 end
