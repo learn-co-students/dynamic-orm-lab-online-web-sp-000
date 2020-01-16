df93ba58d4af0b6125259a1c1483239cafcaed87
Michael Carroll <carrollm2@gmail.com>
2020-01-16 02:56:42 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -60,13 +60,28 @@ class InteractiveRecord
 
   def save
 
-    DB[:conn].execute("INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (?)", [values_for_insert])
+    # DB[:conn].execute("INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (?)", [values_for_insert])
+    DB[:conn].execute("INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})")
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
 
-  def find_by_name(name)
+  def self.find_by_name(name)
     DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE name = ?", [name])
+  end
+
+
+  def self.find_by(attribute)
+
+    attribute_description = ""
+    attribute_value = ""
+
+    attribute.each do |k, v|
+      attribute_description = k
+      attribute_value = v
+    end
+
+    DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE #{attribute_description} = ?", [attribute_value])
   end
 
 end
