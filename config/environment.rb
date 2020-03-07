0de0c1a95be9af5d5503cd5afad5d5d2dd791eb6
ksummerill <kaitlynsummerill@gmail.com>
2020-03-06 18:04:51 -0800

    dynamic orm lab complete

--- config/environment.rb
+++ config/environment.rb
@@ -5,11 +5,13 @@ DB[:conn].execute("DROP TABLE IF EXISTS students")
 
 sql = <<-SQL
   CREATE TABLE IF NOT EXISTS students (
-  id INTEGER PRIMARY KEY, 
-  name TEXT, 
+  id INTEGER PRIMARY KEY,
+  name TEXT,
   grade INTEGER
   )
 SQL
 
 DB[:conn].execute(sql)
 DB[:conn].results_as_hash = true
+
+# results_as_hash method says: when a SELECT statement is executed, don't return a database row as an array, return it as a hash with the column names as keys
