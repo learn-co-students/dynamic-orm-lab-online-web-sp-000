2d2d4e5636b0ddd42e0c40e5869ec95ec14cc3c4
Danielle Bergbower <dkw5011@gmail.com>
2020-03-21 18:13:14 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,5 +3,7 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
-
+self.column_names.each do |col_nam|
+    attr_accessor col_nam.to_sym
+  end
 end
