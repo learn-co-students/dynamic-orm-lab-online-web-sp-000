74200a1d9c4f44d5c7a24b3547110b1db06e225b
C. Leow <chienleow1@gmail.com>
2020-05-03 20:30:44 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,19 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+
+  def self.table_name
+  end
+
+  def self.column_names
+  end
+
+  def initialize
+  end
+
+  def attr_accessor
+  end
+
+
+
 end
