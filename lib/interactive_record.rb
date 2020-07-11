85e3e89630377db2d4779fac58d3fe99a3d1e8fe
Micah <micahmccallum@outlook.com>
2020-07-10 23:41:39 -0400

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,34 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
+
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+    sql = "pragma table_info(#{self.table_name});"
+    table_info = DB[:conn].execute(sql)
+    table_info.collect {|column| column["name"]}.compact
+  end
+   
+  def initialize(attributes={})
+    attributes.each do |property, value|
+      self.send "#{property}=", value
+    end
+  end
+
+  def table_name_for_insert
+    self.class.table_name
+  end
+
+  def col_names_for_insert
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")    
+  end
+
+
+
+
+
   
 end
