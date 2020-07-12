eb94efd88cc07b02d570d80274b3585dc5ccfe4f
Micah <micahmccallum@outlook.com>
2020-07-11 22:47:53 -0400

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -8,14 +8,18 @@ class InteractiveRecord
   end
 
   def self.column_names
-    sql = "pragma table_info(#{self.table_name});"
+    sql = "pragma table_info(#{table_name});"
     table_info = DB[:conn].execute(sql)
-    table_info.collect {|column| column["name"]}.compact
+    column_names = []
+    table_info.each do |row|
+       column_names << row["name"]
+    end
+    column_names.compact
   end
    
   def initialize(attributes={})
-    attributes.each do |property, value|
-      self.send "#{property}=", value
+    attributes.each do |key, value|
+      self.send "#{key}=", value
     end
   end
 
@@ -28,9 +32,29 @@ class InteractiveRecord
   end
 
   def values_for_insert
-    binding.pry
-    
+    values = []
+    self.class.column_names.each do |column_name|
+      values << "'#{send(column_name)}'" unless send(column_name).nil?
+    end
+    values.join(", ")
   end
+
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{table_name} WHERE name = ?"
+    DB[:conn].execute(sql, name)
+  end
+
+  def self.find_by(attribute)
+    binding.pry
+  end
+    
+
 
 
 
