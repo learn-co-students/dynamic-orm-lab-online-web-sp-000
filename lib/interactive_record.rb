e03d980605eeb8f9620a03701f462c16a596640a
applelele <applelele@gmail.com>
2020-09-05 18:41:00 -0400

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -17,20 +17,64 @@ class InteractiveRecord
         end
 
         column_names.compact
-    end
+    end       
 
-    def initialize(hash={})
-        hash.each do |property, value|
+    # self.column_names.each do |col_name|
+    #     attr_accessor col_name.to_sym
+    # end
+
+    def initialize(attributes = {})
+        attributes.each do |property, value|
           self.send("#{property}=", value)
         end
     end
 
-    self.column_names.each do |column_name|
-        attr_accessor column_name.to_sym
+    def table_name_for_insert
+        self.class.table_name
     end
 
-    # sql = <<-SQL
+    def col_names_for_insert
+        self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+        # when we save our Ruby object, we should not include the id column name or insert a value for the id column
+        # turn the array of ["name", "album"] into a comma separated list "name, album"
+    end
+
+    def values_for_insert
+        values = []
+        self.class.column_names.each do |col_name|
+            values << "'#{send(col_name)}'" unless send(col_name).nil?
+        end
+        values.join(", ")
+    end
+
+    def save
+        sql = <<-SQL
+          INSERT INTO #{table_name_for_insert} (#{col_names_for_insert})
+          VALUES (#{values_for_insert})
+        SQL
+        DB[:conn].execute(sql)
+        # DB[:conn].execute("INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})")
+
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+    end
+
+    def self.find_by_name(name)
+        sql = <<-SQL
+          SELECT * from #{table_name}
+          WHERE name = ?
+        SQL
+        DB[:conn].execute(sql, name)
+    end
+
+    def self.find_by(attributes={})
         
-    # SQL
+        value = attributes.values[0]
+        formatted_value = value.class == Fixnum ? value : "#{value}"
+        sql = <<-SQL
+          SELECT * from #{table_name}
+          WHERE #{attributes.keys[0]} = ?
+        SQL
+        DB[:conn].execute(sql, formatted_value)
+    end
   
 end
--- lib/student.rb
+++ lib/student.rb
@@ -2,6 +2,13 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 require 'interactive_record.rb'
 
+# The only code the this class needs to contain is 
+# the code to create the attr_accessors SPECIFIC TO ITSELF.
+
 class Student < InteractiveRecord
+
+    self.column_names.each do |col_name|
+        attr_accessor col_name.to_sym
+    end
 
 end
