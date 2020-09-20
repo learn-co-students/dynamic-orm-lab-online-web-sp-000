5e1a599cd645f3fa3a3f1ed6bcf70d9fde9a60a9
jrog20 <jaimerogstad@gmail.com>
2020-09-19 20:11:43 -0400

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -53,9 +53,9 @@ class InteractiveRecord
     DB[:conn].execute(sql, name)
   end
 
-  def self.find_by(column_names)
-    sql = "SELECT * FROM #{self.table_name} WHERE #{column_names} = ?"
-    DB[:conn].execute(sql, column_names)
+  def self.find_by(grade)
+    sql = "SELECT * FROM #{self.table_name} WHERE grade = ?"
+    DB[:conn].execute(sql, grade)
   end
 
 end
