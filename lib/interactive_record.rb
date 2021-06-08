1da03d49f8cb3563dc99b4979bf6e2afd9e72cd9
Clay Stewart <c.stewart89@gmail.com>
2021-06-08 17:47:22 -0400

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,6 +1,53 @@
-require_relative "../config/environment.rb"
+require_relative '../config/environment.rb'
 require 'active_support/inflector'
+require 'pry'
 
 class InteractiveRecord
-  
+  def initialize(options = {})
+    options.each { |property, value| self.send("#{property}=", value) }
+  end
+
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+    DB[:conn].results_as_hash = true
+
+    sql = "PRAGMA table_info('#{table_name}')"
+
+    table_info = DB[:conn].execute(sql)
+    column_names = []
+
+    table_info.each { |column| column_names << column['name'] }
+
+    column_names.compact
+  end
+
+  def values_for_insert
+    values = []
+    self
+      .class
+      .column_names
+      .each do |col_name|
+        values << "'#{send(col_name)}'" unless send(col_name).nil?
+      end
+    values.join(', ')
+  end
+
+  def save
+    DB[:conn].execute(
+      "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (?)",
+      [values_for_insert][0],
+    )
+
+    @id =
+      DB[:conn].execute(
+        "SELECT last_insert_rowid() FROM #{table_name_for_insert}",
+      )[
+        0
+      ][
+        0
+      ]
+  end
 end
--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,15 @@
-require_relative "../config/environment.rb"
+require_relative '../config/environment.rb'
 require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
+  self.column_names.each { |col_name| attr_accessor col_name.to_sym }
 
+  def table_name_for_insert
+    self.class.table_name
+  end
+
+  def col_names_for_insert
+    self.class.column_names.delete_if { |col| col == 'id' }.join(', ')
+  end
 end
